e318d101167b2b635b1a348b1066d5f7c7cf1c75
fbaezap <baeza.gdo@gmail.com>
2015-12-23 19:07:00 -0300

    Métodos insert sin verificar en cada tabla
    Están los metodos insert. No han sido probados. No se válida que las FK
    existan

--- Usuario.py
+++ Usuario.py
@@ -3,6 +3,10 @@ import MySQLdb
 import DBConnector
 import PasswordHandler
 
+TABLA_NOMBRE = "usuario"
+TABLA_ATRIBUTOS = ["id", "usuario", "password", "correo", "nombre", "nacimiento"]
+
+
 def valid_login(usuario, password):	
 	conn = DBConnector.conectarDB()
 	hash_pass = PasswordHandler.encode(usuario, password)
@@ -13,20 +17,20 @@ def valid_login(usuario, password):
 	cursor.close()
 
 	if len(existe) > 0:
-		user_id = existe[0]["id"]
-		return {"valido":True, "user_id":user_id}
-	return {"valido":False, "user_id":-1}
+		usuario_id = existe[0]["id"]
+		return {"valido":True, "usuario_id":usuario_id}
+	return {"valido":False, "usuario_id":-1}
 
 
-def valid_insert_user(user):
+def valid_insert_usuario(usuario):
 
 	import re
 
-	usuario = user["usuario"]
-	password = user["password"]
-	correo = user["correo"]
-	nombre = user["nombre"]
-	nacimiento = user["nacimiento"]
+	usuario = usuario["usuario"]
+	password = usuario["password"]
+	correo = usuario["correo"]
+	nombre = usuario["nombre"]
+	nacimiento = usuario["nacimiento"]
 
 	salida={}
 	error={}
@@ -44,10 +48,10 @@ def valid_insert_user(user):
 	if patron.match(correo)==None:
 		error["correo"] = (u"No es un correo válido.")
 
-	patron_nombre = "^[a-zA-Z]*[a-zA-Z ]+$"
+	patron_nombre = "^[a-zA-Zá-úÁ-Ú]*[a-zA-Zá-úÁ-Ú ]+$"
 	patron = re.compile(patron_nombre)
 	if patron.match(nombre)==None:
-		error["nombre"] = (u"No es un nombre válido.")
+		error["nombre"] = (u"No es un nombre válido. Solo se permiten letras y espacios.")
 
 	import datetime
 	try:
@@ -78,25 +82,27 @@ def valid_insert_user(user):
 	return salida
 
 
-
-def insert_user(user):
+def insert_usuario(usuario):
 	
-	respuesta = valid_insert_user(user)
+	respuesta = valid_insert_usuario(usuario)
 	
 	if not respuesta["valido"]:
 		return respuesta
 
-	usuario = user["usuario"]
-	password = user["password"]
-	correo = user["correo"]
-	nombre = user["nombre"]
-	nacimiento = user["nacimiento"]
+	usuario = usuario["usuario"]
+	password = usuario["password"]
+	correo = usuario["correo"]
+	nombre = usuario["nombre"]
+	nacimiento = usuario["nacimiento"]
 	conn = DBConnector.conectarDB()
 	hash_pass = PasswordHandler.encode(usuario, password)
 	cursor = conn.cursor()
 	sql = """INSERT INTO usuario 
 				(usuario, password, correo, nombre, nacimiento)
 				VALUES (%s, %s, %s, %s, STR_TO_DATE(%s, %s))"""
+
+	salida = {"valido": True, "error": ""}
+
 	try:
 		affected_count = cursor.execute(sql, [usuario, hash_pass, correo, nombre, nacimiento, '%d/%m/%Y'])
 		conn.commit()
@@ -105,5 +111,41 @@ def insert_user(user):
 	finally:
 		cursor.close()
 	
+	return salida
+
+def select_usuario(usuario_id):
+	conn = DBConnector.conectarDB()
+	cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+	sql = "SELECT id, usuario, password, correo, nombre, nacimiento FROM usuario where id=%s"
+	cursor.execute(sql, [int(usuario_id)])
+	existe = cursor.fetchall()
+	cursor.close()
+
+	if len(existe) > 0:
+		usuario = existe[0]
+		return usuario
+	return None
+
+def update_usuario(usuario):
+	
+	usuario_id = usuario["id"]
+	correo = usuario["correo"]
+	nombre = usuario["nombre"]
+	nacimiento = usuario["nacimiento"]
+	conn = DBConnector.conectarDB()
+	cursor = conn.cursor()
+	sql = """UPDATE usuario SET 
+				correo=%s, nombre=%s, nacimiento=STR_TO_DATE(%s, %s)
+				WHERE id=%s"""
+
 	salida = {"valido": True, "error": ""}
+
+	try:
+		affected_count = cursor.execute(sql, [correo, nombre, nacimiento, '%d/%m/%Y', usuario_id])
+		conn.commit()
+	except Exception as inst:
+		salida = {"valido": False, "error": inst}
+	finally:
+		cursor.close()
+	
 	return salida
