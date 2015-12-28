d91a80f6f8bf5840b74f388067a3eefde946f4c6
fbaezap <baeza.gdo@gmail.com>
2015-12-28 02:35:51 -0300

    mysql azure datos
    mysql azure datos

--- DBConnector.py
+++ DBConnector.py
@@ -3,9 +3,9 @@
 import MySQLdb
 
 
-DB_HOST="localhost"
-DB_USER= "root"
-DB_PASS=""
+DB_HOST="br-cdbr-azure-south-a.cloudapp.net"
+DB_USER= "b270c9370980b1"
+DB_PASS="03d13dd3"
 DB_DATABASE = "musiteca"
 
 
